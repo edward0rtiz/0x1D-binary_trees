e3d8ba8406eda44f071836edfdc2c35f650b3713
jorgezafra94 <947@holbertonschool.com>
2019-12-02 16:44:16 +0000

    [feature] insert node at left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_left - add a node in the left of the parent
+ * if it exists it move down one level and add the new node first
+ * @parent: parent of the specified node
+ * @value: value of the node
+ * Return: NULL if it fails or the new node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
