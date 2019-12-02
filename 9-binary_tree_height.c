c2907770df0fc5f399f58f1e808cd4ef17ea7391
jorgezafra94 <947@holbertonschool.com>
2019-12-02 19:30:23 +0000

    [fix] name variables

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,6 +22,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((lt > rt) ? lt : rt);
+		return ((l > r) ? l : r);
 	}
 }
