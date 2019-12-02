356d976ea44ea87b8962c1d94bc661afd16b0d78
jorgezafra94 <947@holbertonschool.com>
2019-12-02 22:23:07 +0000

    [feature] determine if a tree is full or not

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -15,11 +15,23 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	else
 	{
-		left = tree->left ? binary_tree_is_full(tree->left) : 0;
-		right = tree->right ? binary_tree_is_full(tree->right) : 0;
-		if (left == right)
+		if (tree->left && tree->right)
+		{
+			left = binary_tree_is_full(tree->left);
+			right = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
+			{
+				return (0);
+			}
 			return (1);
+		}
+		else if (!tree->left && !tree->right)
+		{
+			return (1);
+		}
 		else
+		{
 			return (0);
+		}
 	}
 }
