d3017f277179f8422ea47cae26ab57b73c0a30dc
edward0rtiz <931@holbertonschool.com>
2019-12-05 07:14:39 -0500

    [fix] ln22/23 height is now counting the levels inside each node, ln36 variables are adjustes according limits usage lib, ln70 MIN and MAX adjusted

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -19,8 +19,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		}
 		return ((l > r) ? l : r);
 		}
@@ -33,26 +33,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @lower: node that point to the lower node selected.
  * Return: 1 if tree is AVL, 0 if not.
  */
-int bal_avl(const binary_tree_t *tree, int high, int lower)
+int bal_avl(const binary_tree_t *tree, int lower, int high)
 {
 	size_t height_l, height_r, balancer;
 
 	if (tree != NULL)
 	{
-		if (tree->n > high|| tree->n < lower)
+		if (tree->n > high || tree->n < lower)
 		{
 			return (0);
 		}
 		height_l = binary_tree_height(tree->left);
 		height_r = binary_tree_height(tree->right);
-		balancer = height_l > height_r ?
-			height_l - height_r : height_r - height_l;
+		balancer = height_l > height_r ? height_l - height_r : height_r - height_l;
 		if (balancer > 1)
 		{
 			return (0);
 		}
 		return (bal_avl(tree->left, lower, tree->n - 1) &&
-			bal_avl(tree->right, high, tree->n + 1));
+			bal_avl(tree->right, tree->n + 1, high));
 	}
 	return (1);
 }
@@ -65,6 +64,8 @@ int bal_avl(const binary_tree_t *tree, int high, int lower)
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
 	if (tree == NULL)
+	{
 		return (0);
+	}
 	return (bal_avl(tree, INT_MIN, INT_MAX));
 }
