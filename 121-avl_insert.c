420c665cb913dbb94fc74d8ed0d455280bcc78db
edward0rtiz <931@holbertonschool.com>
2019-12-05 16:21:04 -0500

    [fix] performance

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,31 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Function that measures the height of a binary tree
- * @tree: tree to go through
- * Return: the height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t l = 0;
-	size_t r = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		}
-		return ((l > r) ? l : r);
-		}
-}
-
-/**
  * r_insert_node - node value instertion in a AVL.
  * @tree: type **pointer of root node of the AVL tree struct.
  * @parent: parent node of struct AVL.
