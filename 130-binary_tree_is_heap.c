3a970bc7abe125e27afc9b0f608f0bdf766f8c82
edward0rtiz <931@holbertonschool.com>
2019-12-06 12:53:00 -0500

    [fix] add ln 19 condition of NULL and refactor return of 0 in height

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -16,10 +16,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree)
+		if (tree->left == NULL && tree->right == NULL)
 		{
-			l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
 		return ((l > r) ? l : r);
 		}
